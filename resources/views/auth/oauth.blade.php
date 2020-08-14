a72d6c06fd2c63270ade6ebb58bd59c2c342068a
Michael Wagner <wagnmich@gmail.com>
2020-08-14 21:09:12 +0200

    fixed some minor bugs for Release 2

--- resources/views/auth/oauth.blade.php
+++ resources/views/auth/oauth.blade.php
@@ -7,14 +7,13 @@
                 <div class="mb-4 mt-3" style="display: flex; justify-content: space-between;">
                   <h3>OAuth Clients and Token</h3>
                   <div style="text-align: right;">
-                    <button class="btn btn-danger">Delete User and all Data</button>
+                    <a role="button" class="btn btn-danger" href="{{ route('unregister') }}">Delete User and all Data</a>
                   </div>
                 </div>
                 <passport-clients class="mb-3"></passport-clients>
                 <passport-authorized-clients class="mb-3"></passport-authorized-clients>
                 <passport-personal-access-tokens class="mb-3"></passport-personal-access-tokens>
             </div>
-
         </div>
     </div>
 @endsection
